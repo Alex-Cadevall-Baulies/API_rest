a9dada16aa26b5f462abe2010a71e0e2a790e869
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-10-31 20:06:24 +0100

    trying to understand upload

--- Sprint 4-1/routes/imgUpload.js
+++ Sprint 4-1/routes/imgUpload.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router()
 
+const multer = require("multer");
+const upload = multer({ dest: '../views/images'});
+
 router.get('/', function (req, res) {
     res.render('index.ejs');
   });
 
-router.post()
+router.post('/upload', upload.single('image'), (req, res) =>{
+
+})
 
 module.exports = router
