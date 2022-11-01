eb7bb6a224af87c38ab3857c371f20b1af38d1a9
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-11-01 10:42:51 +0100

    nodemon dependency

--- Sprint 4-1/routes/imgUpload.js
+++ Sprint 4-1/routes/imgUpload.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router()
 
 const multer = require("multer");
-const upload = multer({ dest: '../views/images'});
+const upload = multer({ dest: './views/images'});
 
 router.get('/', function (req, res) {
     res.render('index.ejs');
   });
 
-router.post('/upload', upload.single('image'), (req, res) =>{
-
+router.post('/', upload.single('image'), (req, res) =>{
+  console.log(req.file.filename);
+  res.send(201)
 })
 
 module.exports = router
