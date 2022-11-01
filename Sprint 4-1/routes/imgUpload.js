bbe51ab8aa7b759b9fcd78c364af42ead49c9a36
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-11-01 18:35:47 +0100

    still acceptc certain mimetypes

--- Sprint 4-1/routes/imgUpload.js
+++ Sprint 4-1/routes/imgUpload.js
@@ -2,15 +2,43 @@ const express = require('express');
 const router = express.Router()
 
 const multer = require("multer");
-const upload = multer({ dest: './views/images'});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './views/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+});
+
+let fileFilter = function (req, file, cb) {
+  const fileTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+
+    if (fileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({
+  storage: storage,
+  filefilter: fileFilter
+})
 
 router.get('/', function (req, res) {
-    res.render('index.ejs');
-  });
+  res.render('index.ejs');
+});
 
-router.post('/', upload.single('image'), (req, res) =>{
-  console.log(req.file.filename);
-  res.send(201)
+router.post('/', upload.single('file'), (req, res) => {
+  try {
+    console.log(req.file);
+    res.status(201).send(`Document ${req.file.originalname} guardat correctament`)
+  } catch (err) {
+    console.log(err)
+    res.status(400).send(`Document no enviat, prova amb jpg, png o gif`)
+  }
 })
 
 module.exports = router
