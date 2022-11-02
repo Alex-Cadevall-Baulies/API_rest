c48955414966e4dcf5d1fdd810e7d4e5a6898274
Rocio Marchetti <alexcadevall@gmail.com>
2022-11-02 12:06:23 +0100

    routes set-up

---
+++ Sprint 4-2/routes/players.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router()
+
+// POST create player
+// PUT modify player
+// GET return player list
+
+module.exports = router
---
+++ Sprint 4-2/routes/ranking.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router()
+
+// GET ranking organized by individual and average success %
+// GET loser player with less success %
+// GET winner player with most success %
+
+module.exports = router
