1be28ccd2eec3ac3a367792e997259498d78f027
Rocio Marchetti <alexcadevall@gmail.com>
2022-11-08 10:20:17 +0100

    players almost working

--- Sprint 4-2/app.js
+++ Sprint 4-2/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
+const port = 8080;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 const sequelize = require('./utils/database');
 
 const gameInfo = require('./models/gameInfo');
 const playerInfo = require('./models/playerInfo');
 const throws = require('./models/throws');
+
+const gamesRouter = require('./routes/games');
+const playersRouter = require('./routes/players');
+const rankingRouter = require('./routes/ranking');
 
 gameInfo.belongsToMany(playerInfo, { through: throws });
 
@@ -16,4 +22,7 @@ sequelize
     .then((result) => console.log(result))
     .catch((err) => console.log(err))
 
-// 
+
+app.use('/players', playersRouter)
+
+app.listen(port, () => console.log(`Running on http://localhost:${port}`))
--- Sprint 4-2/routes/players.js
+++ Sprint 4-2/routes/players.js
@@ -3,36 +3,45 @@ const router = express.Router()
 
 const playerClass = require('../class/playerClass')
 const playerInfo = require('../models/playerInfo')
-const playerJson = require('../utils/playerJson')
+
+// GET return player list
+router.get('/', async (req, res) => {
+    try {
+        let playerList = await playerInfo.findAll({})
+        res.json(playerList)
+    } catch (err) { console.log(err) }
+});
 
 // POST create player
 router.post('/', async (req, res) => {
     try {
-        let username = req.body
-        const newUser = playerClass(username)
-        const userConfirmation = newUser.createPlayer()
-        res.send(userConfirmation)
+        let newUsername = req.body.username
+        console.log(newUsername)
+        const newUser = new playerClass(newUsername)
+        const userConfirmation = await newUser.createPlayer()
+        console.log(userConfirmation)
+        
+        res.json({
+            id: userConfirmation.player_id,
+            username: userConfirmation.username,
+            register_date: userConfirmation.register_date
+        })
+
     } catch (err) { console.log(err) }
 });
 
 // PUT modify player
 router.put('/:id', async (req, res) => {
     try {
-        let username = req.body
-        const user = playerClass(username)
-        const newUsername = ""
+        let currentUsername = req.params
+        const newUsername = req.body
+        console.log(id, newUsername)
+
+        const user = new playerClass(currentUsername)
         const changeUser = user.modifyPlayer(newUsername)
 
-        res.send(`Usuari ${changeUser.player_id} modificat, nou nom d'usuari ${changeUser.username}`)
+        res.send(`Usuari ${changeUser.player_id} modificat, nou nom d'usuari: ${changeUser.username}`)
 
-    } catch (err) { console.log(err) }
-});
-
-// GET return player list
-router.get('/', async (req, res) => {
-    try {
-        let playerList = await playerInfo.findAll({})
-        res.send(playerList)
     } catch (err) { console.log(err) }
 });
 
